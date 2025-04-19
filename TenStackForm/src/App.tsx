2817b01b45339bd6a77d3a0f08695aa924d05205
Akshat Vyas <akshatmanojvyas@gmail.com>
2025-04-18 16:13:30 -1200

    rahul chages

--- TenStackForm/src/App.tsx
+++ TenStackForm/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Form from './Components/Form'
 
 const App:React.FC = () => {
   return (
-    <div className='bg-red-400 text-center m-auto'>App</div>
+    <div className='bg-red-400 text-center m-auto'><h1>App</h1>
+    <Form/>
+    </div>
   )
 }
 
